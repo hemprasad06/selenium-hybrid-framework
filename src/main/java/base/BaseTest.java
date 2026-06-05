56274d17df1a0684b46d8a17ce36b49e9d041891
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-06-05 22:40:17 +0530

    comitting driverfactory & basetest

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,4 +1,19 @@
 package base;
 
+import driver.DriverFactory;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 public class BaseTest {
+    @BeforeMethod
+    public void setUp()
+    {
+        DriverFactory.initDriver();
+    }
+
+    @AfterMethod
+public void tearDown()
+    {
+        DriverFactory.quitDriver();
+    }
 }
--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -1,4 +1,28 @@
 package driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class DriverFactory {
+
+    public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void initDriver()
+    {
+        WebDriverManager.chromedriver().setup();
+        driver.set(new ChromeDriver());
+    }
+
+    public static WebDriver getDriver()
+    {
+        return driver.get();
+    }
+
+    public static void quitDriver()
+    {
+        driver.get().quit();
+        driver.remove();
+    }
 }
