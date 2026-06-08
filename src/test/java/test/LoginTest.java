adc1bdf11d605e46bad0eeb81e929793eff6dc5e
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-06-08 10:10:49 +0530

    comitting today's code

--- src/test/java/test/LoginTest.java
+++ src/test/java/test/LoginTest.java
@@ -1,4 +1,24 @@
 package test;
 
-public class LoginTest {
+import base.BaseTest;
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import java.sql.Driver;
+
+public class LoginTest extends BaseTest
+{
+@Test
+    public void loginTest()
+{
+    DriverFactory.getDriver().get("https://www.saucedemo.com/");
+    DriverFactory.getDriver().findElement(By.id("user-name")).sendKeys("standard_user");
+    DriverFactory.getDriver().findElement(By.id("password")).sendKeys("secret_sauce");
+    DriverFactory.getDriver().findElement(By.id("login-button")).click();
+
+    String title = DriverFactory.getDriver().getTitle();
+    System.out.println(title);
+
+}
 }
