b3715d3093bd62adbe1cb10bb755bccc19de4ec3
Hem Prasad Saravanan <hemprasad06@gmail.com>
2026-06-14 00:49:59 +0530

    comitting today work

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,26 @@
 package pages;
 
+import driver.DriverFactory;
+import org.openqa.selenium.By;
+
 public class LoginPage {
+    private By usernameTextbox = By.id("username");
+    private By passwordTextbox = By.id("password");
+    private By loginButton = By.id("loginbutton");
+
+    public void enterUsername(String username)
+    {
+        DriverFactory.getDriver().findElement(usernameTextbox).sendKeys(username);
+
+    }
+
+    public void enterpassword(String password)
+    {
+        DriverFactory.getDriver().findElement(passwordTextbox).sendKeys(password);
+    }
+
+    public void clickLoginButton()
+    {
+        DriverFactory.getDriver().findElement(loginButton).click();
+    }
 }
