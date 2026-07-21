bd1bab0214bde80f28536b02b90745fa60de2832
Sandhya0297 <hemprasad06@gmail.com>
2026-07-21 23:01:43 +0530

    comoitting today's works

--- src/main/java/utils/RetryAnalyzer.java
+++ src/main/java/utils/RetryAnalyzer.java
@@ -1,52 +1,22 @@
 package utils;
 
 import org.testng.IRetryAnalyzer;
-import org.testng.ITestResult;
 
-public class RetryAnalyzer implements IRetryAnalyzer {
+public class RetryAnalyzer implements IRetryAnalyzer
+{
+public int intitalCount = 0;
+public int maxRetryCount = 2;
 
-    private int retryCount = 0;
-    private static final int maxRetryCount = 2;
-
-    @Override
-    public boolean retry(ITestResult result) {
-
-        if (retryCount < maxRetryCount) {
-
-            retryCount++;
-
-            System.out.println(
-                    "Retrying Test: "
-                            + result.getName()
-                            + " Attempt: "
-                            + retryCount);
-
-            String screenshotPath =
-                    ScreenshotUtil.captureScreenshot(
-                            result.getName());
-
-            System.out.println(
-                    "Screenshot Captured: "
-                            + screenshotPath);
-
-            if (retryCount == 2) {
-
-                try {
-
-                    System.out.println(
-                            "Applying extended wait before second retry");
-
-                    Thread.sleep(5000);
-
-                } catch (InterruptedException e) {
-
-                    e.printStackTrace();
-                }
-            }
-
-            return true;
-        }
-
-        return false;
+@override
+    public boolean retry(ITestResult result)
+{
+    if(intitalCount < maxRetryCount)
+    {
+        intitalCount++;
+        return true;
     }
+}
+return false;
+
+
 }
